510ebe3031ef19de30e94c28859d35406078b514
Jean-Baptiste Lavisse <jean-baptiste@jblavisse.fr>
2021-05-06 16:21:04 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
+import Thing from './Thing';
 
 class App extends Component {
+  state = {
+    users: []
+  }
+
+  componentDidMount = () => {
+    console.log("Coucou c'est le démarrage de l'application")  
+    this.getUsers();
+  }
+
+
+  getUsers = () => {
+    axios.get('https://randomuser.me/api/?results=9')
+    .then(res => {
+      this.setState({
+        users: res.data.results
+      })
+    })
+  }
+
   render() {
+
+    // Objectif: retourner plutôt un composant <User/>
+    const usersJSX = this.state.users.map(user => {
+      return <li>
+            <div>
+              <p>{user.name.first} {user.name.last}</p>
+              <img src={user.picture.thumbnail} alt=""/>
+            </div>
+        </li>
+    })
+
+
     return (
       <div className="App">
+          <button onClick={this.getUsers}>Charger des utilisateurs</button>
+
+          <ul>
+            {usersJSX}
+          </ul>
+
+          <Thing/>
+          <Thing/>
+
       </div>
     );
   }
---
+++ src/Thing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Thing extends Component {
+    componentDidMount() {
+        console.log("Ouiiiiiiinnnnnnnnn");
+    }
+    
+    render() {
+        return (
+            <div>
+                Brrrrrrr
+            </div>
+        )
+    }
+}
